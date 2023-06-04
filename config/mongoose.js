c6698ce8bb986f382c956e7db340f9d3abdebe32
Priya Kumari <46952823+priyaakumari@users.noreply.github.com>
2023-06-04 17:27:02 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,7 +5,7 @@ const uri = process.env.HABIT_URI;
 
 // local db settingss
 //mongoose.connect('mongodb://127.0.0.1:27017/habit_tracker');
-mongoose.connect(`mongodb+srv://priyaprasad407:aFzmNLKvogdYOmOF@cluster0.venggr4.mongodb.net/$habits?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://priyaprasad407:aFzmNLKvogdYOmOF@cluster0.venggr4.mongodb.net/habits?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
